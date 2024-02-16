b677db25148ab70aa0df61a0830d22ea1f908994
Andrey <61388196+relativemodder@users.noreply.github.com>
2024-02-16 11:49:38 +0300

    Update SafeMode.cpp
    lol

--- src/Hacks/SafeMode.cpp
+++ src/Hacks/SafeMode.cpp
@@ -27,15 +27,15 @@ void updateSafemode()
         }
     }
     
-    Client::GetModule("safe-mode")->enabled = a;
+    Client::GetModule("safe-mode")->enabled = false;
 }
 
 class $modify (PlayLayer)
 {
     TodoReturn levelComplete() {
-        updateSafemode();
+        // updateSafemode();
 
-        if (!Client::GetModuleEnabled("safe-mode"))
+        if (true)
         {
             PlayLayer::levelComplete();
             return;
@@ -46,18 +46,18 @@ class $modify (PlayLayer)
     }
 
     void showNewBest(bool p0, int p1, int p2, bool p3, bool p4, bool p5) {
-        updateSafemode();
+        // updateSafemode();
 
-        if (!Client::GetModuleEnabled("safe-mode"))
+        if (true)
             return PlayLayer::showNewBest(p0, p1, p2, p3, p4, p5);
     }
 };
 
 class $modify(GJGameLevel) {
     void savePercentage(int p0, bool p1, int p2, int p3, bool p4) {
-        updateSafemode();
+        // updateSafemode();
 
-        if (!Client::GetModuleEnabled("safe-mode"))
+        if (true)
             GJGameLevel::savePercentage(p0, p1, p2, p3, p4);
     }
 };
